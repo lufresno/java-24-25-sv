51236e0598261a199106aaa9d69978ffab10ace1
lufresno <navarrofresnolucia@gmail.com>
2024-10-04 12:09:39 +0200

    [04/10/24] 12:09 Java Primer Día

---
+++ Simpson/src/Simpson.java
@@ -0,0 +1,26 @@
+
+public class Simpson {
+    //VISIBLE A NIVEL DE LA CLASE SIMPSON
+    char [][] tablero = new char [10][10];
+    //FIN
+
+    //Software de gestión de venta ONLINE
+    public static void rellenarTablero(char pers){
+
+
+    }
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+        // int contador= 0;
+        char personaje = '#';
+        rellenarTablero(personaje);
+        //contador= 10;
+        rellenarTablero(personaje);
+
+        /*for (int i=0; i<10; i++){
+            for (int j=0; j<10; j++){
+
+            }
+        }*/
+    }
+}
