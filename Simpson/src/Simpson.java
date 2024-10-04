b1afbc3411b8088473de4fab50ac96ea0557d97a
lufresno <navarrofresnolucia@gmail.com>
2024-10-04 12:20:07 +0200

    Update Simpson.java

--- Simpson/src/Simpson.java
+++ Simpson/src/Simpson.java
@@ -1,13 +1,14 @@
 
 public class Simpson {
     //VISIBLE A NIVEL DE LA CLASE SIMPSON
-    char [][] tablero = new char [10][10];
+    static char [][] tablero = new char [10][10];
     //FIN
 
     //Software de gestión de venta ONLINE
     public static void rellenarTablero(char pers){
-
-
+        for (int i=0; i<10; i++){
+            for (int j=0; j<10; j++) tablero[i][j] = pers;
+        }
     }
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
@@ -16,11 +17,5 @@ public class Simpson {
         rellenarTablero(personaje);
         //contador= 10;
         rellenarTablero(personaje);
-
-        /*for (int i=0; i<10; i++){
-            for (int j=0; j<10; j++){
-
-            }
-        }*/
     }
 }
